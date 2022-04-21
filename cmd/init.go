3cf273d07484fa5013104d3747e450e26e0c8c35
abdfnx <abdfnabd@gmail.com>
2022-04-21 21:25:05 +0000

    finish from `gomo init` command

--- cmd/init.go
+++ cmd/init.go
@@ -1,1 +1,20 @@
 package cmd
+
+import (
+	"github.com/abdfnx/gomo/core/initx"
+	"github.com/spf13/cobra"
+)
+
+func InitCMD() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "doctor",
+		Short: "Creates a new gomo.json file in the current folder",
+		Long: "Creates a new gomo.json file in the current folder",
+		Aliases: []string{"."},
+		Run: func(cmd *cobra.Command, args []string) {
+			initx.InitX()
+		},
+	}
+
+	return cmd
+}
